ef8a0ebfc773e326d8f9daec89937b2e38caa824
Vaibhavsurvase1997 <vaibhavsurvase@neosoftmail.com>
2021-09-21 16:53:36 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,39 @@
+var input_box=document.getElementById("calculation");
+var historyData=[];
+var expressionData="";
+var resultData="";
+
+function UserClickButton(input) {
+    var oldinput=input_box.value;
+    var newinput=oldinput+input;
+    expressionData=newinput;
+    input_box.value=newinput;
+}
+function CalculateValue() {
+    console.log("Click");
+    var input=input_box.value;
+    input=input.replace('"',"").replace("'","");
+    var result=eval(input);
+    resultData=result;
+    historyData.push({"expression":expressionData,"result":resultData});
+    showLogdata();
+    resultData="";
+    expressionData="";
+    input_box.value=result;
+}
+function ClearData() {
+    input_box.value="";
+}function backSpace(){
+    screenView.value = screenView.value.slice(0, - 1);
+}
+
+function showLogdata() {
+    var log=document.getElementById("history_log");
+    var string="";
+    for(var key in historyData){
+        string+=""+historyData[key]["expression"]+" = "+historyData[key]["result"]+"<br>";
+    }
+
+
+    log.innerHTML=string;
+}
